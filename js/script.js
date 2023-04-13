777154e5c2e310db65727000ce61a8fff4dc860f
Luiz Guilherme Crema <lgcrema@hotmail.com>
2023-04-13 00:44:37 -0300

    menu hamburguer

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile__menu')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open') 
+        document.querySelector('.icon').src = "../img/menu.png"
     } else {
         menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "../img/Sem título.png"
     }
 }
