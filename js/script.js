5743163866b96badd767d25fbeac0cc2ecca6c0c
Luiz Guilherme Crema <lgcrema@hotmail.com>
2023-04-15 16:00:57 -0300

    atualizacao de projeto

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile__menu')
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open') 
-        document.querySelector('.icon').src = "../img/botaoMenu.png"
+        document.querySelector('.icon').src = "../img/icons8-menu-arredondado-50.png"
     } else {
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "../img/botaoClose.png"
+        document.querySelector('.icon').src = "../img/icons8-excluir-50.png"
     }
 }
